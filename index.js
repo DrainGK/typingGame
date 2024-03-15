bac23d17dfa91ab43ca05712de9050e66856c5c8
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2024-03-15 13:28:01 +0900

    menu, sounds

--- index.js
+++ index.js
@@ -17,15 +17,20 @@ function adjustCanvasForHighDPI(canvas) {
 document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("canvas");
   adjustCanvasForHighDPI(canvas);
-  updateCanvas();
 });
 
 //global variables
 let lastKeyPressed = "";
 let score = 0;
 let shield = 3;
-let isGameOver = false;
-let text = document.getElementById("game-text");
+let isGameOver = true;
+let startTime = 0;
+let currentTimeDisplay;
+let timerIntervalId = null;
+const menu = document.getElementById("menu");
+const start = document.getElementById("start")
+const text = document.getElementById("game-text");
+const time = document.getElementById("time");
 const uiScore = document.getElementById("score");
 const uiShield = document.getElementById("shield");
 const charArray = [];
@@ -47,7 +52,10 @@ let demonTextX = canvas.width * 2; // horizontal axis for demon spell initialize
 let demonTextY = 0.1 + Math.random() * 0.8;
 let animationFrameId = null; // requestAnimationFrame
 
-document.addEventListener("keydown", function zob(e) {
+const sounds = ["assets/game_music.mp3", "assets/spell.mp3", "assets/shield_hit.mp3", "assets/shield_break.mp3", "assets/cancel.mp3"];
+let gameSound;
+
+document.addEventListener("keydown", function(e) {
   if (e.key === "Backspace") {
     e.preventDefault();
     charArray.pop(); //here we are taking of the last object of the array
@@ -56,6 +64,45 @@ document.addEventListener("keydown", function zob(e) {
     charArray.push(lastKeyPressed); //we're adding a new object inside the arrey
   }
 });
+
+start.addEventListener("click", function(){
+  menu.style.display = "none";
+  startGame();
+})
+gameSound = new sound(sounds[0]);
+
+function startGame(){
+  gameSound.play();
+  isGameOver = false;
+  startTime = Date.now();
+  updateTimeDisplay();
+
+  if(timerIntervalId !== null){
+    clearInterval(timerIntervalId);
+  }
+
+  timerIntervalId = setInterval(updateTimeDisplay, 1000);
+
+  charArray.length = 0;
+  score = 0;
+  shield = 3;
+
+  uiShield.innerText = `Shield: ${shield}`;
+
+  updateCanvas();
+}
+
+function updateTimeDisplay(){
+  const elapsedTime = Date.now() - startTime;
+  const totalSeconds = Math.floor(elapsedTime / 1000);
+  const minutes = Math.floor(totalSeconds/60).toString().padStart(2,'0');
+  const seconds = (totalSeconds % 60).toString().padStart(2,'0');
+  currentTimeDisplay = `${minutes}:${seconds}`;
+
+  if (time){
+    time.innerText = currentTimeDisplay;
+  }
+}
 
 function updateCanvas() {
   if (isGameOver) {
@@ -86,6 +133,13 @@ function updateCanvas() {
     ctx.stroke();
   }
 
+  if(shield == 0 ){
+    gameSound = new sound(sounds[3]);
+    gameSound.play();
+    gameSound.stop();
+  
+  }
+
   // Demon text movement logic
   demonTextX -= 7;
   let measuredWidth = ctx.measureText(currentDemonText).width;
@@ -94,7 +148,10 @@ function updateCanvas() {
     gameOver();
   } else if (demonTextX < shieldX && shield > 0) {
     shield--;
+    gameSound = new sound(sounds[2]);
+    gameSound.play();
     updateShieldDisplay();
+    textShield();
     demonTextX = canvas.width;
     demonTextY = 0.1 + Math.random() * 0.8;
     charArray.length = 0;
@@ -114,15 +171,29 @@ function updateCanvas() {
 
 function cancelSpell(text) {
   if (text.toLowerCase() === currentDemonText.toLocaleLowerCase()) {
+    gameSound = new sound(sounds[4]);
+    gameSound.play();
+    cancelText();
     charArray.length = 0;
     currentDemonText = getRandomDemonWord();
     demonTextX = canvas.width;
     demonTextY = 0.1 + Math.random() * 0.8;
-
     updateCanvas();
     score++;
     updateScoreDisplay(); // Update the score display
   }
+}
+
+function textShield() {
+  if (shield === 0) {
+    text.innerText = "Shield is broken";
+  } else {
+    text.innerText = "Shield is breaking";
+  }
+}
+
+function cancelText() {
+  text.innerText = "cancelled succesfully";
 }
 
 function getRandomDemonWord() {
@@ -138,20 +209,37 @@ function updateShieldDisplay() {
   uiShield.innerText = `Shield: ${shield}`; //displaying the score visually incrementing it
 }
 
-function playerAccuracy() {
-  //checking letter by letter[i] inputText & demontext if is equal; using for loop
-  //drawing each letter individually
-  //ctx.fillText(text[i], 10 + i * (baseFontSize * 0.6), 50); // Adjust position as needed
+function sound(src){
+  this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
 }
 
 function gameOver() {
+  if(timerIntervalId !== null){
+    clearInterval(timerIntervalId);
+    timerIntervalId = null;
+  }
+  gameSound.stop();
+  charArray.length = 0;
   isGameOver = true;
   cancelAnimationFrame(animationFrameId);
 
   score = 0;
   shield = 0;
-  charArray.length = 0;
   text.innerText = "You died !!!";
   updateScoreDisplay();
   updateShieldDisplay();
+
+  menu.style.display = "flex";
+
 }
